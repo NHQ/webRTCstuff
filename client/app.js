6799603ef66f15d3a6368f3872d6e18f0ff5a6e8
NHQ SERVER ADMIN <nhq@NHQ.att.net>
2012-11-10 15:15:54 -0800

    errr

--- client/app.js
+++ client/app.js
@@ -1,8 +1,29 @@
-
 var media = require('./media');
+var domready = require('domready');
+var socket = require('./socket');
+var viewer = require('./viewer');
+var player = require('./player');
+var log = window.log = console.log;
 
 module.exports = function(){
 
-  console.log('i am a teacup');
+  domready(init);
+
+  function init(){
+
+    console.log('AHOY');
+
+    var origin = window.location.host.match('localhost') ? '127.0.0.1' : window.location.host
+    
+    window.socket = io.connect('http://'+origin + ':8000');
+
+    socket.on('connected', connected);
+
+  }
+
+  function connected(){
+    log('connected');
+  }
+
 
 }
