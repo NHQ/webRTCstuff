576bb1b50bd9de260c3cc774b4c4b5cbfa355eee
NHQ SERVER ADMIN <nhq@NHQ.att.net>
2012-11-10 22:46:33 -0800

    app

--- client/app.js
+++ client/app.js
@@ -1,29 +1,17 @@
 var media = require('./media');
 var domready = require('domready');
-var socket = require('./socket');
-var viewer = require('./viewer');
-var player = require('./player');
-var log = window.log = console.log;
+var ui = require('./ui');
 
-module.exports = function(){
+module.exports = function(config){
 
   domready(init);
 
   function init(){
 
-    console.log('AHOY');
-
     var origin = window.location.host.match('localhost') ? '127.0.0.1' : window.location.host
     
-    window.socket = io.connect('http://'+origin + ':8000');
+    var socket = window.socket = io.connect('http://'+origin + ':8000');
 
-    socket.on('connected', connected);
-
-  }
-
-  function connected(){
-    log('connected');
-  }
-
+  };
 
 }
